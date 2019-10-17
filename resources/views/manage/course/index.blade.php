33ebc5661a1c06db66873b21960099e50a15e6fc
Piotr <piotr.porzuczek@gmail.com>
2019-10-17 03:19:51 +0200

    Change links in course management

--- resources/views/manage/course/index.blade.php
+++ resources/views/manage/course/index.blade.php
@@ -25,19 +25,19 @@
                             <h4>
                                 <em>{{ $course->name }}</em> - topics: {{ count($course->topics) }}
                             </h4>
-                            <h6><a href="{{ url('/board') }}/{{$course->abbreviation}}/{{$course->password}}">{{ url('/board') }}/{{$course->abbreviation}}/{{$course->password}}</a></h6>
-                            <h6><a href="{{ url('/learn') }}/{{$course->abbreviation}}/{{$course->password}}">{{ url('/learn') }}/{{$course->abbreviation}}/{{$course->password}}</a></h6>
+                            <h6><a href="{{ url('/board') }}/{{$course->abbreviation}}/{{$course->password}}" target="_blank">{{ url('/board') }}/{{$course->abbreviation}}/{{$course->password}}</a></h6>
+                            <h6><a href="{{ url('/learn') }}/{{$course->abbreviation}}/{{$course->password}}" target="_blank">{{ url('/learn') }}/{{$course->abbreviation}}/{{$course->password}}</a></h6>
                         </div>
                         <div class="row mb-2">
-                            <a href="courses/{{ $course->id }}" class="btn btn-info mr-2">Detail</a>
-                            <a href="courses/{{ $course->id }}/edit" class="btn btn-success mr-2">Edit</a>
+                            <a href="courses/{{ $course->id }}" class="btn btn-info mr-2" target="_blank">Detail</a>
+                            <a href="courses/{{ $course->id }}/edit" class="btn btn-success mr-2" target="_blank">Edit</a>
                             <form method="POST" action="courses/{{$course->id}}" onsubmit="return confirm('Do you really want to delete?');">
                                 {!! csrf_field() !!}
                                 <input type="hidden" name="_method" value="DELETE">
                                 <button type="submit" class="btn btn-danger mr-2">Delete</button>
                             </form>
-                            <a href="{{ route('topics.filter', $course->id) }}" class="btn btn-primary mr-2">Topics</a>
-                            <a href="{{ route('quests.filter', $course->id) }}" class="btn btn-warning mr-2">Quests</a>
+                            <a href="{{ route('topics.filter', $course->id) }}" class="btn btn-primary mr-2" target="_blank">Topics</a>
+                            <a href="{{ route('quests.filter', $course->id) }}" class="btn btn-warning mr-2" target="_blank">Quests</a>
                         </div>
                     </div>
                 @endif
