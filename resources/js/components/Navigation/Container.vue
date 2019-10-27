0c658a1556cae44f325df75ece18b8c20d596e45
Piotr <piotr.porzuczek@gmail.com>
2019-10-27 23:41:33 +0100

    Add body element hue change

--- resources/js/components/Navigation/Container.vue
+++ resources/js/components/Navigation/Container.vue
@@ -29,6 +29,29 @@ export default {
   props: {
     backgroundName: { type: String, default: 'overcast' }
   },
+  watch: {
+    isDark: {
+      immediate: true,
+      handler() {
+        this.changeHueOnBody();
+      }
+    },
+    color: {
+      immediate: true,
+      handler() {
+        this.changeHueOnBody();
+      }
+    }
+  },
+  methods: {
+    changeHueOnBody() {
+      if (document && document.body) {
+          document.body.className = this.isDark ?
+          `t-bg-${this.themeColors.primary}-darkest` :
+          '';
+      }
+    }
+  }
 }
 </script>
 
